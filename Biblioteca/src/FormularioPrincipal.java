5306ad64ad0f0b328ced1a605fd156e3fb2e9e4e
Dmargarb <145496969+Dmargarb@users.noreply.github.com>
2024-11-12 16:15:05 +0100

    Update

--- Biblioteca/src/FormularioPrincipal.java
+++ Biblioteca/src/FormularioPrincipal.java
@@ -1,11 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-
 /**
  *
- * @author Luma
+ * @author David
  */
 public class FormularioPrincipal extends javax.swing.JFrame {
 
